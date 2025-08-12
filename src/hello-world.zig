9245a367f5ae6e8d0c41136f77fcc185109e9d39
DamonCharlesRoberts <dev.properly669@passmail.net>
2025-08-11 20:32:24 -0400

    Cleaning up the hello-world script.

--- src/hello-world.zig
+++ src/hello-world.zig
@@ -1,9 +1,7 @@
 //! Hello World from Zig!
 const std = @import("std");
 
-// The main funciton.
 pub fn main() !void {
-    // Write to the stdout.
     const stdout = std.io.getStdOut().writer();
     try stdout.print("hello world!\n", .{});
 }
